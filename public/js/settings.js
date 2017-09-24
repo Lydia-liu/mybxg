e2fad3dd50a839cdf213ce99c5070376b8e90f0e
lydia <lydia@qq.com>
2017-09-25 00:20:49 +0800

    完成省市县三级联动

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template'], function($, template) {
+define(['jquery', 'template','uploadify','region'], function($, template) {
     //调用接口  获取个人信息
     $.ajax({
         type: 'get',
@@ -6,9 +6,29 @@ define(['jquery', 'template'], function($, template) {
         dataType: 'json',
         success: function(data) {
             //console.log(data)
-
             var html = template('settingsTpl', data.result);
             $('#settingsInfo').html(html);
+
+            //处理头像上传
+            $('#upfile').uploadify({
+            	width:120,
+            	height:120,
+            	buttonText:'',
+            	itemTemplate:'<span></span>',
+            	swf:'/public/assets/uploadify/uploadify.swf',
+            	uploader:'/api/uploader/avatar',
+            	fileObjName:'tc_avatar',
+            	onUploadSuccess:function(a,b){
+            	var obj=JSON.parse(b);
+            	$('.preview img').attr('src',obj.result.path);
+            	}
+
+            });
+
+            //处理省市县三级联动
+            $('#pcd').region({
+            	url:'/public/assets/jquery-region/region.json'
+            })
         }
 
     })
